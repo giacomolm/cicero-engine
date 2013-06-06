22a802645ed50d63016f22051040e5002b52cbd7
giacomolm <giacomolm@gmail.com>
2013-06-06 13:46:54 +0000

    Integrazione Mappa alpha

--- Cicero4.1.2/assets/www/js/views/mapView.js
+++ Cicero4.1.2/assets/www/js/views/mapView.js
@@ -1,31 +1,40 @@
-define(["zepto", "underscore", "backbone","handlebars","models/Event","collections/Events","text!templates/mapView.html"],
-    function ($, _, Backbone, Handlebars,Event,Events,template) {
+define(["zepto", "underscore", "backbone","handlebars","models/Event","collections/Events", "leaflet","text!templates/mapView.html" ],
+    function ($, _, Backbone, Handlebars,Event,Events,L,template) {
 
     var mapView = Backbone.View.extend({
 
         template: Handlebars.compile(template),
 
         initialize: function () {
-          //this.model.bind("reset", this.render, this);
-          /*this.mio_evento = new Event({
-        	  name: "Vini3",
-        	  title: "vini titolo2",
-        	  description: "degustazione vini2"
-          });*/
 
           this.miei_eventi = new Events;
           this.miei_eventi.firebase.on('value',this.render,this);
-          //var self = this;
-          //this.miei_eventi.firebase.on('value', function(snapshot){
-          //	    self.render();
-          //	});
-          //this.miei_eventi.add(this.mio_evento);
+          
+          this.sw = new L.LatLng(-85.05113, -179.29687, true);
+          this.ne = new L.LatLng(51.83578, 171.5625, true);
+          this.bounds = new L.LatLngBounds(this.sw, this.ne);
+          this.render();
+
         },
 
         render: function (eventName) {
           $(this.el).empty();
-          this.mio_evento = this.miei_eventi.at(1); 
-          $(this.el).html(this.template(this.mio_evento.toJSON()));
+          //this.mio_evento = this.miei_eventi.at(1); 
+          $(this.el).html(this.template());
+          
+          this.map = L.map('map',{
+              maxBounds: this.bounds,
+              worldCopyJump: true
+          }).setView([-38.82259, 36.91406], 1, true);
+          L.tileLayer('img/map/{z}/{x}/{y}.png', {
+             minZoom: 0,
+             maxZoom: 3,
+          continuousWorld: true,
+             tms: true
+          }).addTo(map);
+          //map.setMaxBounds(bounds);
+          
+          
           return this;
         }
       });
