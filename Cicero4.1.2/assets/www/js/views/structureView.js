5276590badb79fb14a7d43735327da3763ab38ea
giacomolm@gmail.com <giacomolm@gmail.com@b0f10659-622f-832a-e516-79f0490d72c4>
2013-06-11 17:27:00 +0000

    Adding popup menu

--- Cicero4.1.2/assets/www/js/views/structureView.js
+++ Cicero4.1.2/assets/www/js/views/structureView.js
@@ -2,13 +2,23 @@ define(["zepto", "underscore", "backbone", "handlebars","text!templates/structur
     function ($, _, Backbone, Handlebars,template) {
 
     var structureView = Backbone.View.extend({
-        
+        events: {
+            "touchend #menu" : "toggleMenu"
+          },
+          
         template: Handlebars.compile(template),
 
         initialize: function () {
             this.render();
         },
 
+        toggleMenu: function (eventName) {
+            menuBut = document.getElementById('popupMenu');
+            if(menuBut.style.display == 'none')
+               menuBut.style.display = 'block';
+            else menuBut.style.display = 'none';
+        },
+        
         render: function (eventName) {
             $(this.el).empty();
             $(this.el).html(this.template());
