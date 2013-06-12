a8100e5c78d8accb12df3cd253edb883f571be57
giovi.disanto <giovi.disanto@gmail.com>
2013-06-12 16:06:42 +0000

    router inseriti tutti i path delle viste principali! viste inserito codice per il caricamento del template template inseriti solo paragrafi con nome dei template

--- Cicero4.1.2/assets/www/js/router.js
+++ Cicero4.1.2/assets/www/js/router.js
@@ -1,5 +1,5 @@
-define(["zepto", "underscore", "backbone","views/semiStructureView","views/structureView","views/loginView","views/mapView", "views/registerView", "views/searchView"],
-    function ($, _,Backbone,semiStructureView,StructureView,loginView,mapView,registerView, searchView) {
+define(["zepto", "underscore", "backbone","views/semiStructureView","views/structureView","views/loginView","views/mapView", "views/registerView", "views/searchView", "views/favouriteView","views/eventListView","views/poiListView","views/howToReachUsView","views/nearbyPlacesView","views/eventDetailView","views/poiDetailView","collections/Events","collections/Pois"],
+    function ($, _,Backbone,semiStructureView,StructureView,loginView,mapView,registerView,searchView,favouriteView,eventListView,poiListView, howToReachUsView,nearbyPlacesView,eventDetailView,poiDetailView,Events,Pois) {
 
     var AppRouter = Backbone.Router.extend({
 
@@ -8,7 +8,14 @@ define(["zepto", "underscore", "backbone","views/semiStructureView","views/struc
         "login": "login",
         "map": "map",
         "register": "register",
-        "search": "search"
+        "search": "search",
+        "favourite": "favourite",
+        "eventList": "eventList",
+        "poiList": "poiList",
+        "howToReachUs": "howToReachUs",
+        "nearbyPlaces": "nearbyPlaces",
+        "eventDetail/:id": "eventDetail",
+        "poiDetail/:id": "poiDetail"
       },
 
       initialize: function () {
@@ -51,10 +58,47 @@ define(["zepto", "underscore", "backbone","views/semiStructureView","views/struc
           this.changePage(search);
       },
       
+      favourite: function(){
+          var favourite = new favouriteView();
+          this.changePage(favourite);
+      },
+      
+      eventList: function(){
+          var eventList = new eventListView();
+          this.changePage(eventList);
+      },
+      
+      poiList: function(){
+          var poitList = new poiListView();
+          this.changePage(poiList);
+      },
+      
+      howToReachUs: function(){eventDetail
+          var howToReachUs = new howToReachUsView();
+          this.changePage(howToReachUs);
+      },
+      
+      nearbyPlaces: function(){
+          var nearbyPlaces = new nearbyPlacesView();
+          this.changePage(nearbyPlaces);
+      },
+      
+      eventDetail: function(id){
+          var event_model = Events.getByCid(id);
+          var eventDetail = new eventDetailView({model:event_model});
+          this.changePage(eventDetail); 
+      },
+      
+      poiDetail: function(id){
+          var poi_model = Pois.getByCid(id);
+          var poiDetail = new poiDetailView({model:poi_model});
+          this.changePage(poiDetail);
+      },
+      
       changePage: function (page) {
         if(this.currentView) {
            this.currentView.remove();
-      }
+        }
 
         this.currentView = page;
         $('#container').append($(this.currentView.el));
