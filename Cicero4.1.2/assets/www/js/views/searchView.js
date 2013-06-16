62c10552d85a24799246ba841294f3d3aa9bb478
giacomolm <giacomolm@gmail.com>
2013-06-16 10:02:16 +0000

    Adding searchView

--- Cicero4.1.2/assets/www/js/views/searchView.js
+++ Cicero4.1.2/assets/www/js/views/searchView.js
@@ -1,19 +1,28 @@
-define(["zepto", "underscore", "backbone", "handlebars","text!templates/searchView.html"],
-    function ($, _, Backbone, Handlebars,template) {
+define(["zepto", "underscore", "backbone", "handlebars","models/Poi","collections/Pois","text!templates/searchView.html"],
+    function ($, _, Backbone, Handlebars,Poi,Pois,template) {
 
     var searchView = Backbone.View.extend({
 
         template: Handlebars.compile(template),
 
         initialize: function () {
+            this.pois = new Pois();
+            this.pois.firebase.on("value",this.render,this);
             this.render();
         },
 
         render: function (eventName) {
             $(this.el).empty();
-            $(this.el).html(this.template());
+            poiList = [];
+            for(i=0;i<this.pois.length;i++){
+                poi = this.pois.at(i);
+                poiList[i] =  {title : poi.get("name"), description : poi.get("descrizione")};
+            }
+            context = {poi : poiList};
+            $(this.el).html(this.template(context));
             return this;
-        }
+        },
+        
       });
 
     return searchView;
