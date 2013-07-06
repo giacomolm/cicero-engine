a2940e6b22f7224a54e06409f0b8af9cb33be632
giacomolm@gmail.com <giacomolm@gmail.com@b0f10659-622f-832a-e516-79f0490d72c4>
2013-07-06 12:57:33 +0000

    Adding Favourite to search

--- Cicero4.1.2/assets/www/js/collections/Favourites.js
+++ Cicero4.1.2/assets/www/js/collections/Favourites.js
@@ -4,6 +4,15 @@ define(["zepto", "underscore", "backbone", "models/Favourite","backfire"],
     var Favourites = Backbone.Firebase.Collection.extend({
         model: Favourite,
         firebase: new Firebase("https://cicero.firebaseio.com/favourites"),
+        
+        includesCid: function(user, cid){
+            for(i=0; i<this.length; i++){
+                if(this.at(i).get("user") == user && this.at(i).get("id_ref") == cid)
+                    return this.at(i).get("id");
+            }
+            
+            return 0;
+        }
       });
 
     return Favourites;
--- Cicero4.1.2/assets/www/js/views/poiListItemView.js
+++ Cicero4.1.2/assets/www/js/views/poiListItemView.js
@@ -6,13 +6,45 @@ define(["zepto", "underscore", "backbone", "handlebars","models/Poi","collection
         template: Handlebars.compile(template),
         
         events: {
-            "click #itemView": "goToDetails"
+            "click #itemView": "goToDetails",
+            "touchend #add_button": "addToFav",
+            "touchend #remove_button": "removeToFav",
           },
 
         initialize: function () {
             this.model.bind("change", this.render, this);
+            this.favourites = this.options.favourites;
+            this.listenTo(this.favourites, 'add', this.added);
+            this.listenTo(this.favourites, 'remove', this.removed);
         },
 
+        addToFav: function(){
+            user_id = 0;
+            if(this.favourites.includesCid(user_id, this.model.cid)==0)
+                this.favourites.add({user : user_id, id_ref : this.model.cid, type : 'poi'});
+        },
+        removeToFav: function(){
+            user_id = 0;
+            result = this.favourites.includesCid(user_id, this.model.cid);
+            if(result !=0 )
+                this.favourites.remove(this.favourites.get(result));
+        },
+        added: function(){
+            if(document.getElementById('edit')){
+                if( $('#edit').hasClass("ui-button-error")&& this.favourites.includesCid(0, this.model.cid)!=0){
+                    document.getElementById('red#'+this.model.cid).className = "button_red";
+                    document.getElementById('blue#'+this.model.cid).className = "button_blue invisible";
+                }
+            }
+        },
+        removed: function(){
+            if(document.getElementById('edit')){
+                if( $('#edit').hasClass("ui-button-error")&& this.favourites.includesCid(0, this.model.cid)==0){
+                    document.getElementById('red#'+this.model.cid).className = "button_red invisible";
+                    document.getElementById('blue#'+this.model.cid).className = "button_blue";
+                }
+            }
+        },
         render: function (eventName) {
             var poi = this.model.toJSON();
             poi.cid = this.model.cid;
@@ -21,7 +53,13 @@ define(["zepto", "underscore", "backbone", "handlebars","models/Poi","collection
         },
         
         goToDetails: function () {
-            Backbone.history.navigate("poiDetail/" + this.model.cid, {trigger: true});
+            //il controllo serve per evitare il click sul bottone di add/remove nella vista favourite si propaghi sul div che lo contiene
+            if(document.getElementById('edit')){
+              if(! $('#edit').hasClass("ui-button-error"))
+                  Backbone.history.navigate("poiDetail/" + this.model.cid, {trigger: true});
+            } 
+            else Backbone.history.navigate("poiDetail/" + this.model.cid, {trigger: true});
+                
           }
         
       });
