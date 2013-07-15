c48b5700eb70a500f07729ce82dc018316e336ad
giovi.disanto <giovi.disanto@gmail.com>
2013-07-15 22:14:31 +0000

    fix statusbarnotificationmain sostituizione lib backfire introduzione ciceronotifier (in lavorazione)

--- Cicero4.1.2/assets/www/lib/modules/ciceroNotifier.js
+++ Cicero4.1.2/assets/www/lib/modules/ciceroNotifier.js
@@ -1,7 +1,19 @@
-define(['backbone'], function (Backbone) {
+define(['backbone','models/User','collections/Users','models/Event','collections/Events','models/Favourite','collections/Favourites','statusbarnotification'],
+    function (Backbone,User,Users,Event,Events,Favourite,Favourites,Statusbarnotification) {
 
     var notify = function(){
-        window.plugins.statusBarNotification.notify("test", "messaggio di test");
+        var MyEvents = new Events();
+        MyEvents.firebase.once("value",_.bind(function(){
+            //window.plugins.statusBarNotification.notify("notifiche", "notifiche attive");
+            for(i=0; i<MyEvents.length; i++){
+                var MyEvent = MyEvents.at(i);
+                MyEvent.on("change",function(MyEvent){
+                    //window.plugins.statusBarNotification.notify(MyEvent.get("name"), MyEvent.get("description"));
+                })
+            }
+        },this));
+        //window.plugins.statusBarNotification.notify("test", "messaggio di test");
+
     }
 
     return{
