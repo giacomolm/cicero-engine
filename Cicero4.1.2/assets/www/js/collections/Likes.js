577a30f66054dc1d2bcb999b571796c62e4a595f
giacomolm <giacomolm@gmail.com>
2013-07-22 15:36:58 +0000

    Gestione Like sia agli eventi che agli stand!

--- Cicero4.1.2/assets/www/js/collections/Likes.js
+++ Cicero4.1.2/assets/www/js/collections/Likes.js
@@ -4,6 +4,21 @@ define(["zepto", "underscore", "backbone", "models/Like","backfire"],
     var Likes = Backbone.Firebase.Collection.extend({
         model: Like,
         firebase: new Firebase("https://cicero.firebaseio.com/likes"),
+        getLikes: function(user, id, type){
+                for(i=0; i<this.length; i++){
+                    if(this.at(i).get("user") == user && this.at(i).get("id_ref") == id && this.at(i).get("type") == type)
+                        return this.at(i).get("id");
+                }
+                return -1;
+        },
+        getNumLikes: function(id, type){
+            var num = 0;
+            for(i=0; i<this.length; i++){
+                if(this.at(i).get("id_ref") == id && this.at(i).get("type") == type)
+                    num++;
+            }
+            return num;
+        }
       });
 
     return Likes;
--- Cicero4.1.2/assets/www/js/views/poiDetailView.js
+++ Cicero4.1.2/assets/www/js/views/poiDetailView.js
@@ -1,5 +1,5 @@
-define(["zepto", "underscore", "backbone", "handlebars","klass","photoswipe","collections/Favourites","collections/Comments","collections/Medias","views/commentView","text!templates/poiDetailView.html"],
-    function ($, _, Backbone, Handlebars,Klass,Photoswipe,Favourites,Comments,Medias,commentView,template) {
+define(["zepto", "underscore", "backbone", "handlebars","klass","photoswipe","collections/Favourites","collections/Comments","collections/Medias","collections/Likes","views/commentView","text!templates/poiDetailView.html"],
+    function ($, _, Backbone, Handlebars,Klass,Photoswipe,Favourites,Comments,Medias,Likes,commentView,template) {
 
     var poiDetailView = Backbone.View.extend({
 
@@ -16,7 +16,8 @@ define(["zepto", "underscore", "backbone", "handlebars","klass","photoswipe","co
             "click #favourite_logo": "updateFavourite",
             "click #send" : "send",
             "click #commentsHeader": "showComments",
-            "click #login": "login"
+            "click #login": "login",
+            "mousedown #like_button": "manageLikes"
             //"load #img_fav": "checkFavourite"
         },
 
@@ -40,7 +41,8 @@ define(["zepto", "underscore", "backbone", "handlebars","klass","photoswipe","co
                       
             this.medias.firebase.on("value",this.manageMedia,this);
             
-            
+            this.likes = new Likes();
+            this.likes.firebase.on("value",this.checkLikes,this);
         },
 
         checkFavourite: function(medias){
@@ -110,6 +112,57 @@ define(["zepto", "underscore", "backbone", "handlebars","klass","photoswipe","co
             if(this.user!=undefined && $("#Message").val()!=""){
                 this.comments.add({"id_ref" : this.model.id, "type" : "poi", "text" : $("#Message").val(), "user" : this.user.name, "date" : new Date().getTime()});
                 $("#Message").val("");
+            }
+        },
+        
+        checkLikes:function(){
+            if(this.user!=undefined){
+                if(this.likes!=undefined){
+                    //nella variabile result ho l'id se questo esiste, altrimenti -1
+                    var result = this.likes.getLikes(this.user.id,this.model.id,"poi");
+                    if(result != -1){
+                        $(document).on('DOMNodeInsertedIntoDocument', this.showLike, this);
+                        this.showLike();
+                    }
+                }
+            }
+        },
+        
+        manageLikes:function(){            
+            if(this.user!=undefined){
+                if(this.likes!=undefined){
+                    //nella variabile result ho l'id se questo esiste, altrimenti -1
+                    var result = this.likes.getLikes(this.user.id,this.model.id,"poi");
+                    if(result == -1){
+                        $("#like_logo").addClass("pulse");
+                        this.likes.add({"id_ref" : this.model.id, "type" : "poi", "user" : this.user.id});                         
+                        this.showLike();
+                    }
+                    else{
+                        $("#like_logo").removeClass("pulse");
+                        this.likes.remove(this.likes.get(result));
+                        this.removeLike();
+                    }
+                }
+            }
+            else{
+                $('#message').html("You need to login to like elements");
+                $('#message_div').show();
+            }
+        },
+        
+        showLike: function(){
+            if(document.getElementById('like_div')){
+                var likes = new Likes();
+                var num_likes = likes.getNumLikes(this.model.id, "poi");
+                $('#like_div').html(num_likes+" Likes");
+                $(document).off('DOMNodeInsertedIntoDocument', this.showLike);
+            }
+        },
+        
+        removeLike: function(){
+            if(document.getElementById('like_div')){
+                $('#like_div').html("Likes It!");                
             }
         },
         
