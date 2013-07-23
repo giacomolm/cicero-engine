7dc1db18c1f477c3329f4389e9efb372700c3c2e
giovi.disanto@gmail.com <giovi.disanto@gmail.com@b0f10659-622f-832a-e516-79f0490d72c4>
2013-07-23 13:48:22 +0000

    inserimento titolo nelle mie viste

--- Cicero4.1.2/assets/www/js/views/loginView.js
+++ Cicero4.1.2/assets/www/js/views/loginView.js
@@ -16,6 +16,7 @@ define(["zepto","underscore","backbone","handlebars","eventDispatcher","text!tem
         template: Handlebars.compile(template),
 
         initialize: function () {
+            EventDispatcher.trigger("changeTitle","Login");
             EventDispatcher.on("login_error",this.login_error);
             this.render();
         },
